96037a502168b2b764d6fac607421ec557a1c31d
Kaluri Himabindhu <129114144+Hiomio@users.noreply.github.com>
2025-04-06 18:13:03 +0530

    Update header.jsx

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -5,11 +5,10 @@ export default function Header() {
     <div className="header">
       <div className="headerTitles">
         {/* <span className="headerTitleSm">React & Node</span> */}
-        <span className="headerTitleLg">BlogHorizon</span>
+        <span className="headerTitleLg">BlogSphere</span>
       </div>
       <img
-      className="headerImg" src= "https://images.pexels.com/photos/1261728/pexels-photo-1261728.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-
+      className="headerImg" src="https://images.pexels.com/photos/3847645/pexels-photo-3847645.jpeg?auto=compress&cs=tinysrgb&w=600"
       about="alt"
       />
     </div>
